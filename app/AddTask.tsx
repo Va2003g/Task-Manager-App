c4b2ff912c8439ce7e88584bbc661cf2216bc8d4
vansh.gupta3 <vansh.gupta@geekyants.com>
2024-06-14 16:49:15 +0530

    Task Form Designing Completed

--- app/AddTask.tsx
+++ app/AddTask.tsx
@@ -1,26 +1,67 @@
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { hero, TaskPhoto, Google, hamburger } from "./assets";
 import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
 import colors from "@/colors";
+import { useFonts } from "expo-font";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
 
 const AddTask = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
   return (
     <View style={styles.container}>
-      <LinearGradient
-        colors={[
-          colors.formGradient,
-          colors.formGradient2,
-          colors.formGradient3,
-        ]}
-        style={styles.gradient}
-      ></LinearGradient>
-      <Image source={TaskPhoto} style={styles.image} />
-      
-      <Text style={styles.form}>
-        AddTask
-      </Text>
+      <View style={{ flex: 2, alignItems: "center" }}>
+        <LinearGradient
+          colors={[
+            colors.formGradient,
+            colors.formGradient2,
+            colors.formGradient3,
+          ]}
+          style={styles.gradient}
+        ></LinearGradient>
+        <Image source={TaskPhoto} style={styles.image} />
+      </View>
+
+      <View style={styles.formContainer}>
+        <Text>Add New Task</Text>
+        <View style={styles.form}>
+          <Text style={styles.label}>Task</Text>
+          <TextInput placeholder="Task Name" style={styles.input} />
+          <Text style={styles.label}>Category</Text>
+          <TextInput placeholder="Category" style={styles.input} />
+          <Text style={styles.label}>Tags</Text>
+          <TextInput placeholder="Tags" style={styles.input} />
+          <Text style={styles.label}>Time</Text>
+          <TextInput placeholder="Time" style={styles.input} />
+          <Text style={styles.label}>Due Date</Text>
+          <TextInput placeholder="Due Date" style={styles.input} />
+
+          <LinearGradient
+            colors={[colors.taskBtn1, colors.taskBtn2]}
+            style={{ borderRadius: 10, marginHorizontal: 14 }}
+          >
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.btnText}>Add Task</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
+      </View>
     </View>
   );
 };
@@ -31,30 +72,53 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-evenly",
-    alignItems: "center",
   },
   gradient: {
     borderRadius:
       Math.round(
         Dimensions.get("window").width + Dimensions.get("window").height
       ) / 2,
-    width: Dimensions.get("window").width * 0.38,
-    height: Dimensions.get("window").width * 0.38,
+    width: Dimensions.get("window").width * 0.39,
+    height: Dimensions.get("window").width * 0.39,
     backgroundColor: "#f00",
     justifyContent: "center",
     alignItems: "center",
     zIndex: 1,
   },
+  button: {
+    padding: 19,
+    alignItems: "center",
+    borderRadius: 45,
+  },
+  btnText: {
+    color: "white",
+    fontFamily: "Roboto_700Bold",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
   image: {
     width: 150,
     height: 150,
-    top: 9,
+    top: 20,
     position: "absolute",
     zIndex: 2,
   },
-  form: {
+  formContainer: {
     flex: 8,
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  form: {
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 3,
+    padding: 10,
+    borderColor: "rgba(226, 226, 234, 1)",
+  },
+  label: {
+    textTransform: "uppercase",
+    color: colors.dashboardFont,
+    fontFamily: "Roboto_400Regular",
   },
 });
--- colors.tsx
+++ colors.tsx
@@ -1,13 +1,15 @@
 export default {
-    gradientLogin:"rgba(132, 155, 218, 0.5)",
-    gradient2Login:'rgba(77, 98, 179, 0.7)',
-    fontColor:'#232324',
-    btnColor:'#FC5A5A',
-    primaryDashboard:'rgba(82, 100, 204, 0.8)',
-    primaryDashboard2:'rgba(105, 123, 228, 0.9)',
-    secondaryDashboard:'#F1F1F1',
-    dashboardFont:'#92929D',
-    formGradient:'rgba(52, 49, 109, 1)',
-    formGradient2:'rgba(68, 100, 170, 1)',
-    formGradient3:'rgba(130, 126, 225, 1)'
-}
+  gradientLogin: "rgba(132, 155, 218, 0.5)",
+  gradient2Login: "rgba(77, 98, 179, 0.7)",
+  fontColor: "#232324",
+  btnColor: "#FC5A5A",
+  primaryDashboard: "rgba(82, 100, 204, 0.8)",
+  primaryDashboard2: "rgba(105, 123, 228, 0.9)",
+  secondaryDashboard: "#F1F1F1",
+  dashboardFont: "#92929D",
+  formGradient: "rgba(52, 49, 109, 1)",
+  formGradient2: "rgba(68, 100, 170, 1)",
+  formGradient3: "rgba(130, 126, 225, 1)",
+  taskBtn1: "#6B85E6",
+  taskBtn2: "#6895E6",
+};
